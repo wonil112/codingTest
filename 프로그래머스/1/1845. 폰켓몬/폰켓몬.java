09689596ae9a108f8f61337ca0cf6ac6493fcee9
wonil112 <167044237+wonil112@users.noreply.github.com>
2024-07-23 00:44:18 +0900

    [level 1] Title: 폰켓몬, Time: 10.32 ms, Memory: 77.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] nums) {
+        int answer = (nums.length / 2);
+        int[] distinctArray = Arrays.stream(nums).distinct().toArray();
+        if(answer >= distinctArray.length) {
+            return distinctArray.length;
+        } else {
+            return answer; 
+        }
+    }
+}
