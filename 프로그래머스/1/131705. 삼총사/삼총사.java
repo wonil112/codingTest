f335c051818775bce580d48ac926e1cdd1a80654
wonil112 <167044237+wonil112@users.noreply.github.com>
2024-08-01 01:46:47 +0900

    [level 1] Title: 삼총사, Time: 0.05 ms, Memory: 66.4 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/삼총사.java
+++ 프로그래머스/1/131705. 삼총사/삼총사.java
@@ -4,7 +4,8 @@ class Solution {
         
         // 총 13명의 인원, 무작위로 3명을 뽑아서 더했을 때 합이 0
         // 전체를 순회하되, 3명 단위로 순회해야함.
-        // 3중 for문...
+        // 3중 for문....
+        // 개선사항 있는지 확인
         for (int i = 0; i < number.length - 2; i++) {
             for (int j = i + 1; j < number.length - 1; j++) {
                 for (int k = j + 1; k < number.length; k++) {
