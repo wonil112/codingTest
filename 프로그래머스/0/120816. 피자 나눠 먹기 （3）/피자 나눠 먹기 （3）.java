8d491da61ca444c3c2c977847086546daec4ace4
wonil112 <167044237+wonil112@users.noreply.github.com>
2024-11-10 03:53:56 +0900

    [level 0] Title: 피자 나눠 먹기 (3), Time: 0.03 ms, Memory: 76.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(int slice, int n) {
+        
+        if(n % slice == 0) {
+            return n / slice;
+        } else {
+            return (n / slice) + 1;
+        }
+    }
+}
