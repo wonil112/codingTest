34c07f7a9345e4f4f0b392b0133d47ef5c821f30
Wonil <167044237+wonil112@users.noreply.github.com>
2024-12-28 20:56:34 +0900

    [level 0] Title: 조건에 맞게 수열 변환하기 3, Time: 5.97 ms, Memory: 161 MB -BaekjoonHub

---
+++ 프로그래머스/0/181835. 조건에 맞게 수열 변환하기 3/조건에 맞게 수열 변환하기 3.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] solution(int[] arr, int k) {
+        
+        if(k % 2 == 1) {
+            for (int i = 0; i < arr.length; i++) {
+                arr[i] *= k;
+            }
+        } else {
+            for (int i = 0; i < arr.length; i++) {
+                arr[i] += k;
+            }
+        }
+        
+        return arr;
+    }
+}
