aab5f09ce324ad46059fdf0c2b1434b224d4d7bd
Wonil <167044237+wonil112@users.noreply.github.com>
2024-12-30 23:55:30 +0900

    [level 0] Title: 이어 붙인 수, Time: 18.65 ms, Memory: 80.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int[] num_list) {
+        String odd = "";
+        String even = "";
+
+        for(int i = 0; i < num_list.length; i++) {
+            if(num_list[i] % 2 == 0) {
+                even += num_list[i];
+            } else {
+                odd += num_list[i];
+            }
+        }
+
+        return Integer.parseInt(even) + Integer.parseInt(odd);
+    }
+}
