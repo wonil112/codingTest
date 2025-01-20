99ca6c0d7e7138aa650ca89d61ce5bdf1dc76e24
Wonil <167044237+wonil112@users.noreply.github.com>
2025-01-20 09:16:12 +0900

    [level 0] Title: 합성수 찾기, Time: 0.10 ms, Memory: 71.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120846. 합성수 찾기/합성수 찾기.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        
+        for(int i = 1; i <= n; i++) {
+            if(divisor(i) >= 3) {
+                answer++;
+            }
+        }
+        return answer;
+    }
+    
+     public int divisor(int n) {
+        int count = 0;
+         
+        for(int i = 1; i <= n; i++) {
+            if(n % i == 0) {
+                count++;
+            }
+        } 
+         
+        return count;
+    }
+}
